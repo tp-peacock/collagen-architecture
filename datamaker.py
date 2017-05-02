b29bb437d55ec68ae0a9e143fec0364b26ae95a9
Thomas Peacock <newlocaluser@imac11.complex.ucl.ac.uk>
2017-05-02 20:13:57 +0100

    progress

--- datamaker.py
+++ datamaker.py
@@ -1,3 +1,8 @@
+from __future__ import division
+import math
+
+
+
 """
 -------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------
@@ -6,20 +11,28 @@ Changeable Initial Conditions
 """
 
 # Options - specify name, number of component, and number of types of component
-Atoms = ["atoms", 20, 1]
+Atoms = ["atoms", 24, 3]
 Bonds = ["bonds", 0, 0]
 Angles = ["angles", 0, 0]
 Dihedrals = ["dihedrals", 0, 0]
 Impropers = ["impropers", 0, 0]
 
 # Provide the masses for each type of atom. Masses array should have the same no. of elements as
-# there a
-Masses = [1]
+# there are atom types
+Masses = [1, 1, 1]
 
-#Change the box boundaries
+# Change the box boundaries
 X_bounds = [-10.0,11.0]
 Y_bounds = [-12.0,13.0]
 Z_bounds = [-14.0,15.0]
+
+# Monomer properties
+Monomer_length = 12
+Distance_between_atoms = 1
+Monomer_type_pattern = [1, 2, 3]   # define the types for each atom up to the length or a monomer, or a repeating pattern that forms a subset
+							 # of a monomer
+Monomer_charge_pattern = [1] # define the charges for each atom up to the length or a monomer, or a repeating pattern that forms a subset
+							 # of a monomer
 
 """
 -------------------------------------------------------------------------------------------------------
@@ -58,11 +71,32 @@ def box_config(box):
 		counter+=1
 	return config
 
+def monomer_builder(atoms, monomer_length, overlap, type_pattern, charge_pattern):
+	atom_details = []
+	for a in range(1, atoms[1]+1):
+		atom_config = [a]
+		embed()
+		molecule = math.ceil(a/monomer_length)
+		atom_config.append(molecule)
+
+		atom_type = type_pattern[0]
+		atom_config.append(atom_type)
+		
+		atom_details.append(atom_config)
+
+	
+	return 0
+
+
 def main():
 	#Global Checks
 	if len(Masses) != Atoms[2]:
 		print "Error! Number of Masses and Atom Types do not match!\n"
 		return 0
+
+	if Atoms[1]%Monomer_length != 0:
+		print "Error! Number of atoms does not match Monomer length.\n"
+
 
 	f = open('config.data', 'w')
 
@@ -89,6 +123,8 @@ def main():
 	for line in mass_options:
 		f.write(line)
 	f.write("\n")
+
+	monomer_builder(Atoms, Monomer_length, Distance_between_atoms, Monomer_type_pattern, Monomer_charge_pattern)
 
 	f.close()
 

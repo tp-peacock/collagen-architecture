e278206ecb92b73f3c85e9c74f512ef83faeb2cc
Tom Peacock <thomas.s.peacock@gmail.com>
2017-05-09 23:59:45 +0100

    infinite lines

--- collagen.py
+++ collagen.py
@@ -3,7 +3,18 @@ from IPython import embed
 import lammpsbuilder as lb
 import random
 import math
+import numpy as np
 
+class Box:
+
+	def __init__(self,xlo=-50,xhi=50,ylo=-50,yhi=50,zlo=-50,zhi=50):
+
+		self.xlo = xlo
+		self.xhi = xhi
+		self.ylo = ylo
+		self.yhi = yhi
+		self.zlo = zlo
+		self.zhi = zhi
 
 def randomSphere(r):
 	x = random.uniform(-1,1)
@@ -27,11 +38,11 @@ def voxelise(dim,vox_size):
 	return dim
 
 
-def collagenMonomer(length,xlo,xhi,ylo,yhi,zlo,zhi,data,mononomerId,cutoff):
+def collagenMonomer(length,box,data,mononomerId,cutoff):
 
-	x_0 = random.uniform(xlo,xhi)
-	y_0 = random.uniform(ylo,yhi)
-	z_0 = random.uniform(zlo,zhi)
+	x_0 = random.uniform(box.xlo,box.xhi)
+	y_0 = random.uniform(box.ylo,box.yhi)
+	z_0 = random.uniform(box.zlo,box.zhi)
 
 	delta = randomSphere(cutoff)
 
@@ -56,30 +67,126 @@ def collagenMonomer(length,xlo,xhi,ylo,yhi,zlo,zhi,data,mononomerId,cutoff):
 	return 0
 
 
-
-def redistribution(data,length):
+def findEnds(data, length):
 	ends = []
 	for atom in data.atoms:	
 		if atom.atomId%length == 1 or atom.atomId%length == 0:
-			ends.append(atom)
+			ends.append(atom)	
+	return ends
 
 
+def closestApproach(ends):
+
+	P0 = [ends[0].x,ends[0].y,ends[0].z]
+	Pe = [ends[1].x,ends[1].y,ends[1].z]
+	Q0 = [ends[2].x,ends[2].y,ends[2].z]
+	Qe = [ends[3].x,ends[3].y,ends[3].z]
+
+	u = []
+	v = []
+	xp = Pe[0]-P0[0]
+	yp = Pe[1]-P0[1]
+	zp = Pe[2]-P0[2]
+	xq = Qe[0]-Q0[0]
+	yq = Qe[1]-Q0[1]
+	zq = Qe[2]-Q0[2]
+
+	for i in range(3):
+		u.append((Pe[i]-P0[i])/math.sqrt( xp*xp + yp*yp + zp*zp ))
+
+	for i in range(3):
+		v.append((Qe[i]-Q0[i])/math.sqrt( xq*xq + yq*yq + zq*zq ))
+
+	a = u[0]*u[0] + u[1]*u[1] + u[2]*u[2]
+	b = u[0]*v[0] + u[1]*v[1] + u[2]*v[2]
+	c = v[0]*v[0] + v[1]*v[1] + v[2]*v[2]
+	d = u[0]*(P0[0]-Q0[0]) + u[1]*(P0[1]-Q0[1]) + u[2]*(P0[2]-Q0[2])
+	e = v[0]*(P0[0]-Q0[0]) + v[1]*(P0[1]-Q0[1]) + v[2]*(P0[2]-Q0[2])
+
+	u_np = np.array(u)
+	v_np = np.array(v)
+	P0_np = np.array(P0)
+	Q0_np = np.array(Q0)
+
+	closest = (P0_np - Q0_np) + ((b*e - c*d)*u_np - (a*e -b*d)*v_np)/(a*c - b*b)
+
+	abs_dist = np.linalg.norm(closest)
+
+def cApproach(atom1, atom2, atom3, atom4):
+
+	P0 = np.array([atom1.x, atom1.y, atom1.z])
+	Pe = np.array([atom2.x, atom2.y, atom2.z])
+	Q0 = np.array([atom3.x, atom3.y, atom3.z])
+	Qe = np.array([atom4.x, atom4.y, atom4.z])
+
+	u = (Pe - P0)/np.linalg.norm(Pe-P0)
+	v = (Qe - Q0)/np.linalg.norm(Qe-Q0)
+
+	a = u.dot(u)
+	b = u.dot(v)
+	c = v.dot(v)
+	d = u.dot(P0-Q0)
+	e = v.dot(P0-Q0)
+
+	closest = np.linalg.norm( (P0 - Q0) + ((b*e - c*d)*u - (a*e -b*d)*v)/(a*c - b*b) )
+
+	return closest
+
+
+def findCloseAtoms(atom1, atom2,ends,data,cutoff,monomer_length,box):
+
+	close_atoms = []
+
+	flag = False
+	while flag == False:
+		for i in range(2, len(ends),2):
+			
+			flag = True
+			closest = cApproach(atom1, atom2, ends[i], ends[i+1])
+		
+			if closest <= cutoff*2:
+				close_atoms.append(i*monomer_length/2)
+				redistribute(data, box, i*monomer_length/2,cutoff,monomer_length)
+				ends = findEnds(data,monomer_length)
+				flag = False
+				break
+
+	print "close atoms: ", close_atoms
+	return close_atoms
+
+
+def redistribute(data, box, atom0,cutoff,monomer_length):
+#	noise = random.randint()*cutoff
+	idcount = data.atoms[atom0].atomId
+	print "before: ", data.atoms[atom0]
+	moleculeId = data.atoms[atom0].moleculeId
+	for j in range(atom0, atom0+monomer_length):
+		# data.atoms[i].x -= noise
+		# data.atoms[i].y -= noise
+		# data.atoms[i].z -= noise
+		del data.atoms[atom0]
+
+	collagenMonomer(monomer_length,box,data,moleculeId,cutoff)
+
+	for k in range(len(data.atoms)-monomer_length,len(data.atoms)):
+		data.atoms[k].atomId = idcount
+		idcount+=1
+
+	print "after : ", data.atoms[2970]
+	print "redistributed"
+
+	#embed()
 
 
 def main():
 	
-	xlo = -50
-	xhi = 50
-	ylo = -50
-	yhi = 50
-	zlo = -50
-	zhi = 50
+	box = Box()
 
 	excl_zone = 1.12 #should be the lj cut off
 
 	monomer_length = 30
 
-	data = lb.LammpsData(atomTypes=1, xlo = xlo, xhi = xhi, ylo = ylo, yhi = yhi, zlo = zlo, zhi = zhi)
+	data = lb.LammpsData(atomTypes=1, xlo = box.xlo, xhi = box.xhi, ylo = box.ylo, yhi = box.yhi, zlo = box.zlo, zhi = box.zhi)
 	data.addMass(1,1.0)
 
 	#resize box which atoms can be placed in to a multiple of the voxel size (note: does not change actual box size of simulation)
@@ -87,9 +194,29 @@ def main():
 
 	for i in range(100):
 		# collagenMonomer(30,xlo,xhi,ylo,yhi,zlo,zhi,data,i,excl_zone)
-		collagenMonomer(monomer_length,xlo,xhi,ylo,yhi,zlo,zhi,data,i,excl_zone)
+		collagenMonomer(monomer_length,box,data,i,excl_zone)
 
-	redistribution(data, monomer_length)
+	ends = findEnds(data, monomer_length)
+
+	atom1 = ends[0]
+	atom2 = ends[1]
+	close_atoms = findCloseAtoms(atom1,atom2,ends,data,excl_zone,monomer_length,box)
+
+	ends = findEnds(data, monomer_length)
+
+	atom1 = ends[0]
+	atom2 = ends[1]
+	close_atoms = findCloseAtoms(atom1,atom2,ends,data,excl_zone,monomer_length,box)
+
+	ends = findEnds(data, monomer_length)
+	atom1 = ends[0]
+	atom2 = ends[1]
+	close_atoms = findCloseAtoms(atom1,atom2,ends,data,excl_zone,monomer_length,box)
+
+	#redistribute(close_atoms)
+
+
+
 
 	f = open('lammps.data', 'w')
 
